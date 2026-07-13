c003327c02e6333cab271c893c89fa151bd3b4e4
Carlos Gonzalez <gonsa.carlos@gmail.com>
2026-07-13 10:45:12 +0200

    chore: bump version to v1.0.3

--- cmd/go-stream-tunnel/main.go
+++ cmd/go-stream-tunnel/main.go
@@ -26,7 +26,7 @@ const banner = `
 \____/\____/  /_/  \____/_/       \__/\__,_/_/ /_/_/ /_/\___/_/
 
 `
-const version string = "v1.0.2"
+const version string = "v1.0.3"
 
 const usage1 string = `Usage: go-stream-tunnel server|client|ca [OPTIONS]
 Options:
